45e0168e64b1c9473b0e20521e770ec0df10b685
HuoShengdi <huoshengdi@gmail.com>
2017-01-24 18:42:27 -0800

    edited thank-you message styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'order/:id/feedback' => 'order_feedback#new'
+  get 'order/:id/feedback/complete' => 'order_feedback#new'
 
   namespace :api, defaults: {format: :json} do
     resources :order, only:[:show] do
